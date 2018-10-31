405ac7faea006eb4e6727fe1036c774aa0dc522a
Robin <34440972+Robin-Pooppillikudiyil@users.noreply.github.com>
2018-10-31 12:45:15 +0530

    Update ReadStudentDemo.java

--- 177.solution-code-hibernate-crud/src/com/springlearn/hibernate/demo/ReadStudentDemo.java
+++ 177.solution-code-hibernate-crud/src/com/springlearn/hibernate/demo/ReadStudentDemo.java
@@ -22,7 +22,7 @@ public class ReadStudentDemo {
 		try {			
 			// create a student object
 			System.out.println("Creating new student object...");
-			Student tempStudent = new Student("Daffy", "Duck", "daffy@luv2code.com");
+			Student tempStudent = new Student("Robin", "Thomas", "robin@gmail.com");
 			
 			// start a transaction
 			session.beginTransaction();
